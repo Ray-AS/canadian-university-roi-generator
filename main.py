98674fe55f5e6d0aa5bc168e9bdc61a808f3d0a6
Ray-AS <abisuthans.business@gmail.com>
2026-01-27 18:18:07 -0500

    move constants into configs.py; add weighted roi calculation; add earnings_per_dollar calculation

--- main.py
+++ main.py
@@ -9,8 +9,12 @@ from configs import (
     CPI_ADJUSTMENT_2020_TO_2024,
     EARNINGS_FIELD_MAP,
     ENROLLMENTS_FIELD_MAP,
+    INCOME_TO_PAYOFF,
+    RATE_OF_EARNING_GROWTH,
     STAT_CAN_TABLES,
+    TAX_RATE,
     TUITION_FIELD_MAP,
+    YEARS_OF_TUITION,
     YEARS_TO_KEEP,
 )
 import matplotlib.pyplot as plt
@@ -241,19 +245,19 @@ def merge_dfs(
     )
 
 
-def calculate_basic_roi(df: pd.DataFrame) -> pd.DataFrame:
-    df["total_tuition"] = df["tuition"] * 4
+def calculate_roi_by_field(df: pd.DataFrame) -> pd.DataFrame:
+    df["total_tuition"] = df["tuition"] * YEARS_OF_TUITION
     df["debt_to_income"] = df["estimated_debt"] / df["earnings_2024_adjusted"]
     pass
 
     # Payback years (assuming 10% of post-tax income, 25% tax rate)
-    post_tax = df["earnings_2024_adjusted"] * 0.75
-    annual_payment = post_tax * 0.10
+    post_tax = df["earnings_2024_adjusted"] * (1 - TAX_RATE)
+    annual_payment = post_tax * INCOME_TO_PAYOFF
     df["payback_years"] = df["estimated_debt"] / annual_payment
 
     # 5-year ROI (assume 3% annual salary growth)
     df["earnings_5yr"] = df["earnings_2024_adjusted"] * (
-        1.03**3
+        RATE_OF_EARNING_GROWTH**3
     )  # 3 more years of growth
     avg_earnings = (df["earnings_2024_adjusted"] + df["earnings_5yr"]) / 2
     cumulative_5yr = avg_earnings * 5
@@ -264,7 +268,15 @@ def calculate_basic_roi(df: pd.DataFrame) -> pd.DataFrame:
         "total_tuition"
     ]
 
+    df["earnings_per_dollar_tuition"] = (
+        df["earnings_2024_adjusted"] / df["total_tuition"]
+    )
+
     return df
+
+
+def calculate_roi_all_students(df: pd.DataFrame) -> float:
+    return (df["roi_5yr_w_tuition"] * df["enrollment"]).sum() / df["enrollment"].sum()
 
 
 def main():
@@ -322,7 +334,7 @@ def main():
         debts_by_field,
     )
 
-    merged_w_roi = calculate_basic_roi(merged)
+    merged_w_roi = calculate_roi_by_field(merged)
 
     print("-----------------------TUITION-----------------------")
     print(prepared_tuition_data)
